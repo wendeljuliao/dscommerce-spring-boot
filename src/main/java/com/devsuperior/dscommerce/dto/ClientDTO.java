09c089dadc28959029c9304a64df51150ad995db
Wendel <wendelljuliao@hotmail.com>
2024-01-26 10:03:52 -0300

    Implementing equals and hashcode in client

--- src/main/java/com/devsuperior/dscommerce/dto/ClientDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ClientDTO.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscommerce.dto;
 
+import java.util.Objects;
+
 import com.devsuperior.dscommerce.entities.User;
 
 public class ClientDTO {
@@ -23,6 +25,23 @@ public class ClientDTO {
 
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClientDTO other = (ClientDTO) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 }
