e43fb94d2fe2ebb5169ab706a6ae19ed7fa9410a
Wendel <wendelljuliao@hotmail.com>
2024-01-26 10:05:03 -0300

    implementing equals and hashcode in user

--- src/main/java/com/devsuperior/dscommerce/dto/UserDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/UserDTO.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.dto;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.security.core.GrantedAuthority;
 
@@ -51,6 +52,23 @@ public class UserDTO {
 
 	public List<String> getRoles() {
 		return roles;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDTO other = (UserDTO) obj;
+		return Objects.equals(email, other.email);
 	}
     
 }
